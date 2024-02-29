0b1e840805fc9b1720adf941363f3541c8cff3da
Baber Ibrar <baberibrar@gmail.com>
2024-02-29 12:24:01 +0500

    Insert records into the table

---
+++ Insert_Records.sql
@@ -0,0 +1,15 @@
+-- Insert records into the table
+INSERT INTO Books(BookId, Title, PublishedYear, Author)
+VALUES (1, 'An introduction to SQL', 2020, 'XYZ')
+, (2, 'An introduction to Python', 2020, 'ABC')
+, (3, 'An introduction to Java', 2020, 'PQR')
+, (4, 'An introduction to C#', 2020, 'LMN')
+, (5, 'An introduction to C++', 2020, 'DEF');
+
+
+INSERT INTO Students
+VALUES (1, 'Baber', 'Ibrar', 20, 'Class X'),
+(2, 'Ali', 'Ahmed', 21, 'Class X'),
+(3, 'Ahmed', 'Khan', 22, 'Class F'),
+(4, 'Sara', 'Khan', 23, 'Class T'),
+(5, 'Sana', 'Khan', 24, 'Class Z');
